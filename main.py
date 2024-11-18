8449fbcb3fa4bb11ebd7f0d00974d8553432b2e0
Mayur Dayal <mayur.dayal97@gmail.com>
2024-11-18 20:18:02 +0530

    feat(app): added PyPi Scrapper execution

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import logging
+
 from app.scrapper import PyPiScrapper
 
 
 def main():
+    # initialize logging
+    logger = logging.getLogger(__name__)
+    logger.info("Starting PyPi Scrapper")
+
     url = "https://pypi.org/search/?&o=-created&c=Programming+Language+%3A%3A+Python"
 
     scrapper = PyPiScrapper(url)
-    scrapper.scrape_all_projects()
+
+    try:
+        scrapper.scrape_all_projects()
+    except Exception as e:
+        logger.exception(f"An unexpected error occurred: {e}")
+    finally:
+        logger.info("PyPi Scrapper has completed execution.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
