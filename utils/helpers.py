5de887992249b5d73b128715d338313dcb27e5d3
Mayur Dayal <mayur.dayal97@gmail.com>
2024-11-19 00:35:19 +0530

    chore(dependencies): downgraded aiohttp and httpx versions

--- utils/helpers.py
+++ utils/helpers.py
@@ -5,7 +5,7 @@ def get_headers():
     """Return headers that mimic a real browser."""
     user_agent = UserAgent()
 
-    headers = {"User-Agent": user_agent.random(),
+    headers = {"User-Agent": user_agent.random,
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
